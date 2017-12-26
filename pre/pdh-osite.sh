8f1b44c3cd4055f6f3e27417ee59568a9e4b7d75
Kenji Nishimura <kenji.q246r@gmail.com>
2017-12-26 16:43:44 +0900

    O-site and T-site modeler changed

--- pre/pdh-osite.sh
+++ pre/pdh-osite.sh
@@ -1,5 +1,5 @@
 # PdHx: H located at O-site
-atomsk --create fcc 3.885 Pd -duplicate 1 1 1 pdh.atsk
-atomsk pdh.atsk -add-atom H at 0.5*box 0.0*box 0.0*box pdh.lmp
+atomsk --create fcc 3.885 Pd -duplicate 2 2 2 pdh.atsk
+atomsk pdh.atsk -add-atom H at 1.9425 0.0 0.0 pdh.lmp
 cp pdh.lmp data.file
 rm pdh.atsk pdh.lmp
