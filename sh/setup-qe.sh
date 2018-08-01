4e25db8b5a9c5a9e4bbeb5f65cde63657e079195
Kenji Nishimura <kenji.q246r@gmail.com>
2018-08-01 18:11:29 +0900

    sh/setpu-qe.sh corrected

--- sh/setup-qe.sh
+++ sh/setup-qe.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 
 # Compile Quantum ESPRESSO
-# $ mkdir ~/bin
 # Execute your home directory
+# Set path myqe/bin
 
+#sudo apt install -y libblas-dev liblapack-dev
 git clone https://github.com/QEF/q-e.git myqe
 
 cd myqe
 ./configure
 make all
-
-#cp lmp_mpi ~/bin/lmp_mpi_new
