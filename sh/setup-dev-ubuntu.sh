222ce8035d659e08d9b7571cebd246bf819fb3f5
Kenji Nishimura <kenji.q246r@gmail.com>
2018-08-11 15:52:20 +0900

    sh/setup-dev-ubuntu.sh changed

--- sh/setup-dev-ubuntu.sh
+++ sh/setup-dev-ubuntu.sh
@@ -3,4 +3,6 @@
 # Install development environment in Ubuntu
 # $ sudo -s
 
-apt install -y gcc gfortran openmpi-bin libopenmpi-dev make g++
+apt install -y gcc gfortran g++ make
+apt install -y openmpi-bin libopenmpi-dev
+apt install -y libblas-dev liblapack-dev
