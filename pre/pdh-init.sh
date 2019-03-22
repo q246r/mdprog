72e6e292525f319400aace202a3ad33da71c37a7
Kenji Nishimura <kenji.q246r@gmail.com>
2019-03-22 11:24:16 +0900

    pre/pdh-init.sh modified

--- pre/pdh-init.sh
+++ pre/pdh-init.sh
@@ -1,5 +1,4 @@
 # PdHx
 atomsk --create rocksalt 3.885 Pd H -duplicate 10 10 10 pdh.atsk
 atomsk pdh.atsk -select random 50.0% H -rmatom select pdh.lmp
-cp pdh.lmp data.file
-rm pdh.atsk pdh.lmp
+rm pdh.atsk
