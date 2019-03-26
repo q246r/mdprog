c8d81c8394e6b0c0c5d947f48b0a68a7f36d54e7
Kenji Nishimura <kenji.q246r@gmail.com>
2019-03-26 16:25:24 +0900

    post/stress.sh modified

--- post/stress.sh
+++ post/stress.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 fname="stress.d"
-dt=0.0001
-b2p=0.0001
+fname1="stress1.d"
 
-grep -v "#" $fname | awk '{print $1*0.0001,$2*0.0001}'
+echo "# t(ps), s11(GPa), s22, s33, s12, s13, s23" > $fname1
+grep -v "#" $fname | awk '{print $1*0.0001,$2*0.0001,$3*0.0001,$4*0.0001,$5*0.0001,$6*0.0001,$7*0.0001}' >> $fname1
