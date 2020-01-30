b82b561df01bee4574edafeb60fbc070ef657238
Kenji Nishimura <kenji.q246r@gmail.com>
2020-01-30 10:42:51 +0900

    sh/set-git.sh added

---
+++ sh/set-git.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+git config --global user.name "Kenji Nishimura"
+git config --global user.email "kenji.q246r@gmail.com"
+
+git config --global color.ui true
+
+git config --global alias.st status
+git config --global alias.cm commit
+git config --global alias.co checkout
+git config --global alias.br branch
