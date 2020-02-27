981576d428c19bc5a4033988be3560c2598f6b6d
Kenji Nishimura <kenji.q246r@gmail.com>
2020-02-27 15:35:08 +0900

    sh/setup-nfs-client.sh modified

--- sh/setup-nfs-client.sh
+++ sh/setup-nfs-client.sh
@@ -2,10 +2,12 @@
 
 nfs_server="150.29.122.90"
 
+yum install rpcbind nfs-utils
+
 mkdir /data
 chmod 777 data
 
-mount -vt nfs $nfs_server:/data /data
+mount -t nfs $nfs_server:/data /data
 
 cp /etc/fstab /etc/fstab.back
 echo "$nfs_server:/data /data nfs defaults 0 0" >> /etc/fstab
