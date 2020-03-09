f9a0029b4e13d44fb5892beec7db33518cfb032f
Kenji Nishimura <kenji.q246r@gmail.com>
2020-03-09 17:28:35 +0900

    Bugfix: sh/setup-nis-client.sh

--- sh/setup-nis-client.sh
+++ sh/setup-nis-client.sh
@@ -6,7 +6,7 @@ ypdomainname surface.world
 cp /etc/sysconfig/network /etc/sysconfig/network.backup
 echo "NISDOMAIN=surface.world" >> /etc/sysconfig/network
 
-autoconfig --enablenis --nisdomain=surface.world --nisserver=vasp001.a09.aist.go.jp --enablemkhomedir --update
+authconfig --enablenis --nisdomain=surface.world --nisserver=vasp001.a09.aist.go.jp --enablemkhomedir --update
 
 systemctl start rpcbind ypbind
 systemctl enable rpcbind ypbind
