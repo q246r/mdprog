47b3af26b1eb426467c522d38aac4c7a1e253d87
Kenji Nishimura <kenji.q246r@gmail.com>
2020-09-30 11:21:46 +0900

    hello.cpp renewed

--- sh/hello.cpp
+++ sh/hello.cpp
@@ -1,17 +1,8 @@
 #include <iostream>
-#include <mpi.h>
-#include <stdio.h>
 
-int main(int argc, char **argv)
-{
-	int rank, size;
-	MPI_Init(&argc, &argv);
+using namespace std;
 
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	MPI_Comm_size(MPI_COMM_WORLD, &size);
-
-	std::cout << "rank = " << rank << std::endl;
-	std::cout << "size = " << size << std::endl;
-
-	MPI_Finalize();
+int main(){
+ cout << "Hello world." << endl;
+ return 0;
 }
