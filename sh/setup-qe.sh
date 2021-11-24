17f8ac1a3edad1cf2f8c33040848cc3a48b49a32
Kenji Nishimura <kenji.q246r@gmail.com>
2021-11-24 17:04:38 +0900

    sh/setup-qe.sh changed

--- sh/setup-qe.sh
+++ sh/setup-qe.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 
-# Compile Quantum ESPRESSO
-# Execute your home directory
-# Set path myqe/bin
-
-#sudo apt install -y libblas-dev liblapack-dev
+sudo apt install -y libblas-dev liblapack-dev
 git clone https://github.com/QEF/q-e.git myqe
 
 cd myqe
-./configure
-make all
+
+# GNU compiler
+./configure F90=gfortran F77=gfortran CC=gcc MPIF90=mpif90 --enable-openmp --enable-parallel
+# Intel Parallel Studio XE
+#./configure MPIF90=mpiifort F90=ifort F77=ifort CC=gcc --enable-openmp --enable-parallel --with-scalapack=inte
+# Intel OneAPI
+#./configure MPIF90=mpiifort F90=ifort F77=ifort CC=icc --enable-openmp --enable-parallel --with-scalapack=intel
+
+make -j 4 all
