3d991723e016f3e37f12466bdaf5c756e796110b
Kenji Nishimura <kenji.q246r@gmail.com>
2024-01-03 14:38:24 +0900

    sh/setup-lammps.sh is modified.

--- sh/setup-lammps.sh
+++ sh/setup-lammps.sh
@@ -3,9 +3,7 @@
 git clone -b stable https://github.com/lammps/lammps.git mylammps
 
 cd mylammps/src
-make yes-kspace
 make yes-manybody
-make yes-molecule
 make -j 4 mpi
 
 # cp lmp_mpi ~/bin/lmp_mpi_new
